b5e8d706fff9657f16bae7e3316ad2174a76c051
chaitanya <chaitanyaprakash.n@gmail.com>
2020-04-16 02:41:36 +0530

    Adding country name also to the response

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ function parseAndSendResponse(res, data, callback) {
 
   const response = {
     updatedAt: updatedAt,
+    name: 'India',
     cases: {
       active: numActive,
       cured: numCured,
